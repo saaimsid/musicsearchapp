a01fb007b7e2169356c102b6b3f4c09c5d987866
saaimsid <31106802+saaimsid@users.noreply.github.com>
2020-09-07 18:49:47 +0530

    Add files via upload

---
+++ precache-manifest.acb5bea22e90ddc74161f1e259eacf63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed71c9a72092e990cfa74b6e87aa7936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f1f4f49dbefb474ae70",
+    "url": "/static/css/main.8cb969e3.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1f4f49dbefb474ae70",
+    "url": "/static/js/main.31781658.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
